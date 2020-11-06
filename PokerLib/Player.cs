a48bae7cf326351b27909d584ce8987f99e4a284
Iteipnilia <emma.ailinpieti@live.se>
2020-11-06 22:24:24 +0100

    player

--- PokerLib/Player.cs
+++ PokerLib/Player.cs
@@ -3,12 +3,29 @@ namespace Poker
     class Player
     {
         private string name;
+        private int wins;
         private Hand hand;
-        private Card[] dicard;
+        private Card[] discard;
 
-        public Player()
+        public Player(string name_)
         {
+            name=name_;
+            wins=0;
+            hand= new Hand();
+            discard= new Card[5];
 
+        }
+
+        // anropar metod i Hand för att tömma index på hand som ska bort
+        // LÄgger till det borttagna kortet i array discard
+        public void DiscardCard(int index)
+        {            
+           discard[index] = hand.RemoveCardFromHand(index);
+        }
+
+        public void ReceiveCards(Card card)
+        {
+            hand.AddCardToHand(card);
         }
     }
 }
