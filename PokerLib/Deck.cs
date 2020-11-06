bda938b7859b68d654575fc3b98a6de47e91a00d
Iteipnilia <emma.ailinpieti@live.se>
2020-11-06 13:15:56 +0100

    done?

--- PokerLib/Deck.cs
+++ PokerLib/Deck.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Poker
 {
     class Deck
@@ -7,22 +9,57 @@ namespace Poker
 
         public Deck()
         {
+            cards= new Card[52];
 
+            for (int suite = 0; suite < 4;  suite++)// Använd en foreach istället??
+            {
+                for (int rank = 2; rank < 15; rank++)
+                {
+                    cards[suite * 13 + rank - 2]  = new Card((Suite)suite,(Rank)rank);
+                }
+            }
         }
 
-        public Card[] GetTopCard()
+        public Card GetTopCard()
         {
-            return null;
+            int i=0;
+            Card drawnCard;
+            do
+            {
+               i++;
+            }
+            while(cards[i] ==null);
+            
+            drawnCard= cards[i];
+            cards[i]=null; // blir den tom???
+            
+            return drawnCard;
         }
 
         public void PutBackCard(Card card)
         {
-
+            for(int i=0; i< cards.Length; i++)
+            {
+                if(cards[i]==null)
+                {
+                    cards[i]=card;
+                }
+            }
         }
-
-        public void Shuffle()
+        
+        //Flytta Shuffle() till Dealer??
+        public void Shuffle()//Fisher-Yates
         {
+            Random random= new Random();
 
+            for (int i = cards.Length - 1; i > 0; i--)
+            {
+                 int randomIndex = random.Next(0, i + 1);
+ 
+                Card temp = cards[i];// temporärt kort = 1a index
+                cards[i] = cards[randomIndex]; // kort på 1a index blir kortet på random index
+                cards[randomIndex] = temp; // kort på random index blir kortet från 1a index
+            }
         }
     }
 }
