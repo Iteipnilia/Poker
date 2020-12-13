274ad66da932b9ebc0a435059aa2a88fda311835
Linus Wester <LWester19@gmail.com>
2020-12-13 11:54:32 +0100

    TGF LoadGame

--- Poker.Lib.UnitTest/TestGameFactory.cs
+++ Poker.Lib.UnitTest/TestGameFactory.cs
@@ -3,6 +3,7 @@ using Poker;
 using System;
 using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using static Poker.Suite;
@@ -31,6 +32,33 @@ namespace Poker.Lib.UnitTest
         [Test]
         public void CanCreateLoadGame()
         {
+            IPokerGame game;
+            Table table = new Table();
+            string fileName = "savedGame.txt";
+            string[] names = new string[2] { "Test1", "Test2" };
+            int[] wins = new int[2] { 7, 3 };
+
+            string json = JsonConvert.SerializeObject(names);
+            json += (" " + JsonConvert.SerializeObject(wins));
+            File.WriteAllText(fileName, json);
+
+            string Json = File.ReadAllText(fileName);
+            string[] data = Json.Split(' ');
+            string[] Names = JsonConvert.DeserializeObject<String[]>(data[0]);
+            int[] Wins = JsonConvert.DeserializeObject<int[]>(data[1]);
+
+            for (int i = 0; i < names.Length; i++)
+                {
+                    table.AddPlayerToTable(names[i], wins[i]);
+                }
+
+
+            game = GameFactory.LoadGame(fileName);
+
+            Assert.AreEqual(game.Players[0].Name, "Test1");
+            Assert.AreEqual(game.Players[1].Name, "Test2");
+            Assert.AreEqual(game.Players[0].Wins, 7);
+            Assert.AreEqual(game.Players[1].Wins, 3);
         
         }
     }
